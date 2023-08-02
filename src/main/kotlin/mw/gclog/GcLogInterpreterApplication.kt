713f266a9129d9cac2125070a91ee4ec9aeda099
Mariusz Wojcik <mariusz.wojcik@allegropay.pl>
2023-08-02 19:54:34 +0200

    gc log interpreter - init

---
+++ src/main/kotlin/mw/gclog/GcLogInterpreterApplication.kt
@@ -0,0 +1,81 @@
+package mw.gclog
+
+import com.microsoft.gctoolkit.GCToolKit
+import com.microsoft.gctoolkit.io.GCLogFile
+import com.microsoft.gctoolkit.io.SingleGCLogFile
+import mw.gclog.aggregation.G1DataAggregation
+import org.jetbrains.kotlinx.dataframe.io.writeCSV
+import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.time.Duration
+import java.time.Instant
+import kotlin.jvm.optionals.getOrDefault
+
+
+private var initialMarkCount = 0
+private var remarkCount = 0
+private var defNewCount = 0
+
+//@SpringBootApplication
+class GcLogInterpreterApplication
+
+fun main(args: Array<String>) {
+    //val machine = GcLogAnalyzer()
+    //machine.run()
+    //runApplication<GcLogInterpreterApplication>(*args)
+    val gcLogFile = "/Users/mariusz.wojcik/workspace/git-repo/gc-log-interpreter/gclogs/2b3a8d4c-3d59-474e-b2a1-6030e4b7027d.log"
+
+    require(!gcLogFile.isBlank()) { "This sample requires a path to a GC log file." }
+
+    require(!Files.notExists(Path.of(gcLogFile))) { String.format("File %s not found.", gcLogFile) }
+
+    analyze(gcLogFile)
+}
+
+@Throws(IOException::class)
+fun analyze(gcLogFile: String?) {
+    val start = Instant.now()
+
+    /**
+     * GC log files can come in  one of two types: single or series of rolling logs.
+     * In this sample, we load a single log file.
+     * The log files can be either in text, zip, or gzip format.
+     */
+    val logFile: GCLogFile = SingleGCLogFile(Path.of(gcLogFile))
+    val gcToolKit = GCToolKit()
+    /**
+     * This call will load all implementations of Aggregator that have been declared in module-info.java.
+     * This mechanism makes use of Module SPI.
+     */
+    /*gcToolKit.loadAggregationsFromServiceLoader()
+    gcToolKit.loadAggregation(HeapOccupancyAfterCollectionSummary())
+    gcToolKit.loadAggregation(PauseTimeSummary())
+    gcToolKit.loadAggregation(CollectionCycleCountsSummary())*/
+
+    gcToolKit.loadAggregation(G1DataAggregation())
+    /**
+     * The JavaVirtualMachine contains the aggregations as filled out by the Aggregators.
+     * It also contains configuration information about how the JVM was configured for the runtime.
+     */
+    val machine = gcToolKit.analyze(logFile)
+
+    // Retrieves the Aggregation for HeapOccupancyAfterCollectionSummary. This is a time-series aggregation.
+    val message = ""
+    machine.getAggregation(G1DataAggregation::class.java)
+            .map { it.aggregations }
+            .getOrDefault(emptyMap()).forEach { (gcType, dataSet) ->
+                print { "The XYDataSet for $gcType contains $dataSet.size() items.\n" }
+            }
+
+    val df = machine.getAggregation(G1DataAggregation::class.java).get().dataFrame()
+    df.writeCSV("/Users/mariusz.wojcik/workspace/git-repo/gc-log-interpreter/gclogs/pauses.csv")
+
+    val stop = Instant.now()
+
+    val appDuration = Duration.between(start, stop).toMillis()
+
+    println(" ====> Analysis finished after $appDuration ms <======")
+
+}
+
---
+++ src/main/kotlin/mw/gclog/aggregation/G1DataAggregator.kt
@@ -0,0 +1,124 @@
+package mw.gclog.aggregation
+
+import com.microsoft.gctoolkit.aggregator.*
+import com.microsoft.gctoolkit.event.GarbageCollectionTypes
+import com.microsoft.gctoolkit.event.g1gc.G1GCConcurrentEvent
+import com.microsoft.gctoolkit.event.g1gc.G1GCEvent
+import com.microsoft.gctoolkit.event.g1gc.G1GCPauseEvent
+import org.jetbrains.kotlinx.dataframe.api.toDataFrame
+
+@Aggregates(EventSource.G1GC)
+class G1DataAggregator(results: G1DataAggregation) :
+        Aggregator<G1DataAggregation>(results) {
+    init {
+        register(G1GCPauseEvent::class.java) { event: G1GCPauseEvent -> this.extractHeapOccupancy(event) }
+        register(G1GCConcurrentEvent::class.java) { event: G1GCConcurrentEvent -> this.extractHeapOccupancy(event) }
+    }
+
+    private fun extractHeapOccupancy(event: G1GCEvent) {
+        aggregation().addDataPoint(event.garbageCollectionType, event)
+    }
+}
+
+@Collates(G1DataAggregator::class)
+class G1DataAggregation : Aggregation() {
+
+    val aggregations: MutableMap<GarbageCollectionTypes, G1DataPoints> = mutableMapOf()
+    val eventDataFrame: Map<String, MutableList<Any>> = initialDataFrame()
+
+    fun addDataPoint(gcType: GarbageCollectionTypes, event: G1GCEvent) {
+        aggregations.getOrPut(gcType) { G1DataPoints() }.add(event)
+        when (event) {
+            is G1GCConcurrentEvent -> toDataFrame(event)
+            is G1GCPauseEvent -> toDataFrame(event)
+        }
+    }
+
+    override fun hasWarning(): Boolean {
+        return false
+    }
+
+    override fun isEmpty(): Boolean {
+        return false
+    }
+
+    private fun initialDataFrame() = mapOf<String, MutableList<Any>>(
+            "type" to mutableListOf(),
+            "event_type" to mutableListOf(),
+            "startedAt" to mutableListOf(),
+            "duration" to mutableListOf(),
+            "cause" to mutableListOf(),
+            "cpu_user" to mutableListOf(),
+            "cpu_kernel" to mutableListOf(),
+            "cpu_wallClock" to mutableListOf(),
+            "heap_occupancy_before_coll" to mutableListOf(),
+            "heap_occupancy_after_coll" to mutableListOf(),
+            "heap_size_before_coll" to mutableListOf(),
+            "heap_size_after_coll" to mutableListOf(),
+            "perm_or_meta_occupancy_before_coll" to mutableListOf(),
+            "perm_or_meta_occupancy_after_coll" to mutableListOf(),
+            "perm_or_meta_size_before_coll" to mutableListOf(),
+            "perm_or_meta_size_after_coll" to mutableListOf()
+    )
+
+
+    fun dataFrame() = eventDataFrame.toDataFrame()
+
+
+    private fun toDataFrame(event: G1GCPauseEvent) {
+        eventDataFrame.forEach { (k, v) ->
+            when (k) {
+                "type" -> v.add(event.garbageCollectionType)
+                "event_type" -> v.add("PauseEvent")
+                "startedAt" -> v.add(event.dateTimeStamp)
+                "duration" -> v.add(event.duration)
+                "cause" -> v.add(event.gcCause)
+                "cpu_user" -> v.add(event.cpuSummary?.user ?: 0)
+                "cpu_kernel" -> v.add(event.cpuSummary?.kernel ?: 0)
+                "cpu_wallClock" -> v.add(event.cpuSummary?.wallClock ?: 0)
+                "heap_occupancy_before_coll" -> v.add(event.heap?.occupancyBeforeCollection ?: 0)
+                "heap_occupancy_after_coll" -> v.add(event.heap?.occupancyAfterCollection ?: 0)
+                "heap_size_before_coll" -> v.add(event.heap?.sizeBeforeCollection ?: 0)
+                "heap_size_after_coll" -> v.add(event.heap?.sizeAfterCollection ?: 0)
+                "perm_or_meta_occupancy_before_coll" -> v.add(event.permOrMetaspace?.occupancyBeforeCollection ?: 0)
+                "perm_or_meta_occupancy_after_coll" -> v.add(event.permOrMetaspace?.occupancyAfterCollection ?: 0)
+                "perm_or_meta_size_before_coll" -> v.add(event.permOrMetaspace?.sizeBeforeCollection ?: 0)
+                "perm_or_meta_size_after_coll" -> v.add(event.permOrMetaspace?.sizeAfterCollection ?: 0)
+            }
+
+        }
+    }
+
+    private fun toDataFrame(event: G1GCConcurrentEvent) {
+        eventDataFrame.forEach { (k, v) ->
+            when (k) {
+                "type" -> v.add(event.garbageCollectionType)
+                "event_type" -> v.add("ConcurrentEvent")
+                "startedAt" -> v.add(event.dateTimeStamp)
+                "duration" -> v.add(event.duration)
+                "cause" -> v.add(event.gcCause)
+                "cpu_user" -> v.add(0)
+                "cpu_kernel" -> v.add(0)
+                "cpu_wallClock" -> v.add(0)
+                "heap_occupancy_before_coll" -> v.add(0)
+                "heap_occupancy_after_coll" -> v.add(0)
+                "heap_size_before_coll" -> v.add(0)
+                "heap_size_after_coll" -> v.add(0)
+                "perm_or_meta_occupancy_before_coll" -> v.add(0)
+                "perm_or_meta_occupancy_after_coll" -> v.add(0)
+                "perm_or_meta_size_before_coll" -> v.add(0)
+                "perm_or_meta_size_after_coll" -> v.add(0)
+            }
+
+        }
+    }
+
+}
+
+
+data class G1DataPoints(val dataSeries: MutableList<G1GCEvent> = mutableListOf()) {
+    fun add(point: G1GCEvent) = dataSeries.add(point)
+    fun size() = dataSeries.size
+}
+
+
